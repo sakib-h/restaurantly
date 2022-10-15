ae5f0702f4b64205721e1169542e76e06b35c92f
Sakib Hasan <sakib100.sa@gmail.com>
2022-10-15 15:53:15 +0600

    home section design complete

--- src/Containers/Home.jsx
+++ src/Containers/Home.jsx
@@ -3,6 +3,7 @@ import { FiPlay } from "react-icons/fi";
 import { IoMdClose } from "react-icons/io";
 import ReactPlayer from "react-player";
 import { ClipLoader } from "react-spinners";
+import { motion } from "framer-motion";
 const Home = () => {
 	const [isOpen, setIsOpen] = useState(false);
 	const [loading, setLoading] = useState(false);
@@ -14,18 +15,18 @@ const Home = () => {
 			setLoading(false);
 		}, 2000);
 	};
-	console.log(loading);
+
 	return (
 		<>
 			<section
-				className="home container flex items-center justify-between"
+				className="home container flex flex-col  lg:flex-row items-center justify-center lg:justify-between"
 				id="home">
-				<div className=" flex flex-col items-start justify-center h-screen">
-					<h1 className="font-Poppins text-[#ffffff] text-[48px] lg:text-[52px] 2xl:text-[58px] leading-[56px] font-[700] pb-3">
+				<div className=" flex flex-col items-center lg:items-start justify-center  pb-10 lg:pb-0">
+					<h1 className="font-Poppins text-center lg:text-start  text-[#ffffff] text-[48px] lg:text-[52px] 2xl:text-[58px] leading-[56px] font-[700] pb-3">
 						Welcome to{" "}
 						<span className="text-[#cda45e]">Restaurantly</span>
 					</h1>
-					<h2 className="font-Play-fair text-[#eeeeee] text-[22px] pb-10">
+					<h2 className="font-Play-fair text-center lg:text-start  text-[#eeeeee] text-[22px] pb-10">
 						Delivering great food for more than 8 years!
 					</h2>
 					<div className="flex items-center text-[#ffffff]">
@@ -49,7 +50,7 @@ const Home = () => {
 				<div className="bg-[#000000e6] w-full h-screen fixed top-0 left-0">
 					<ClipLoader
 						color="#ffffff"
-						size="24"
+						size="24px"
 						className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
 					/>
 				</div>
@@ -70,17 +71,18 @@ const Home = () => {
 						<ReactPlayer
 							url="https://www.youtube.com/watch?v=u6BOC7CDUTQ"
 							controls={true}
-							width="50%"
-							height="45%"
+							width="560px"
+							height="315px"
 							playing={true}
-							style={{
-								position: "absolute",
-								top: "50%",
-								left: "50%",
-								transform: "translate(-50%,-50%)",
-								border: "none",
-								outline: "none",
-							}}
+							className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] border-none outline-none"
+							// style={{
+							// 	position: "absolute",
+							// 	top: "50%",
+							// 	left: "50%",
+							// 	transform: "translate(-50%,-50%)",
+							// 	border: "none",
+							// 	outline: "none",
+							// }}
 						/>
 					</div>
 				</>

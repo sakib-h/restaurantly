9f84086f1b1f2e2c008b4a5218a4428a77a547b4
Sakib Hasan <sakib100.sa@gmail.com>
2022-10-20 05:49:58 +0600

    ui complete

--- src/Containers/Menu.jsx
+++ src/Containers/Menu.jsx
@@ -19,7 +19,7 @@ const Menu = () => {
 					</h1>
 				</div>
 
-				<div className="flex flex-row justify-center items-center">
+				<div className="flex flex-row justify-center items-center pb-20">
 					{["all", "starters", "salad", "specialty"].map(
 						(item, index) => (
 							<li
@@ -36,12 +36,31 @@ const Menu = () => {
 						)
 					)}
 				</div>
-				<div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+				<div className="grid grid-cols-1 gap-10 lg:grid-cols-2">
 					{menuItems.map((item) => (
 						<div key={item.id}>
 							<div className="flex flex-row justify-between items-start">
-								<div>
-									<img src={item.img} alt="" />
+								<div className="pr-5">
+									<img
+										src={item.img}
+										alt={item.title}
+										className="w-[70px] h-[70px] rounded-[50%] border-[5px] border-[#ffffff33]"
+									/>
+								</div>
+								<div className="flex-1">
+									<div className="flex relative flex-row justify-between dotBg">
+										<h4 className="cursor-pointer hover:text-primary transition-all ease-out duration-300 font-Open-sans text-[12px] 2xl:text-[14px] font-[700] bg-[#1a1814]  pr-3 z-[3]">
+											{item.title}
+										</h4>
+										<p className="text-primary font-[600] font-Open-sans textNormal bg-[#1a1814] pl-3 z-[3]">
+											{item.price}
+										</p>
+									</div>
+									<div>
+										<p className="italic textNormal text-[#ffffff80] font-Poppins">
+											{item.ingredient}
+										</p>
+									</div>
 								</div>
 							</div>
 						</div>

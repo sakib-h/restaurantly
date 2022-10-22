ff69b0d176cd262665fa847e71eb60c40515170c
Sakib Hasan <sakib100.sa@gmail.com>
2022-10-22 14:40:57 +0600

    form design is complete

--- src/Containers/Events.jsx
+++ src/Containers/Events.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -6,23 +6,37 @@ import { Pagination, Autoplay } from "swiper";
 import event1 from "../Assets/Images/event-custom.jpg";
 import event2 from "../Assets/Images/event-birthday.jpg";
 import event3 from "../Assets/Images/event-private.jpg";
-const Events = () => {
-	return (
-		<section id="events" className="relative py-32 text-[#ffffff]">
-			<div className="container">
-				<div className="title mb-[40px]">
-					<div className="flex flex-row items-center justify-start">
-						<h2 className="text-Poppins textNormal uppercase pr-3">
-							Events
-						</h2>
-						<div className="border-t-2 border-[#ffffff33] w-[120px]"></div>
-					</div>
-					<h1 className="font-Play-fair text-[28px] lg:text-[32px]  2xl:text-[36px] text-primary font-bold leading-[54px]">
-						Organize Your Events in our Restaurant
-					</h1>
-				</div>
 
-				<div>
+const Events = () => {
+	// declaring refs
+
+	const nameRef = useRef();
+	const emailRef = useRef();
+	const phoneRef = useRef();
+	const dateRef = useRef();
+	const timeRef = useRef();
+	const peopleRef = useRef();
+	const messageRef = useRef();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+	return (
+		<section id="events" className=" text-[#ffffff]">
+			<div className="events relative py-32">
+				<div className="container">
+					<div className="title mb-[40px]">
+						<div className="flex flex-row items-center justify-start">
+							<h2 className="text-Poppins textNormal uppercase pr-3">
+								Events
+							</h2>
+							<div className="border-t-2 border-[#ffffff33] w-[120px]"></div>
+						</div>
+						<h1 className="font-Play-fair text-[28px] lg:text-[32px]  2xl:text-[36px] text-primary font-bold leading-[54px]">
+							Organize Your Events in our Restaurant
+						</h1>
+					</div>
+
 					<Swiper
 						pagination={{
 							clickable: true,
@@ -30,15 +44,14 @@ const Events = () => {
 						modules={[Pagination, Autoplay]}
 						loop={true}
 						autoplay={{ delay: 3000, disableOnInteraction: false }}
-						speed={1000}
-						className="mySwiper">
+						speed={1000}>
 						<SwiperSlide>
-							<div className="flex flex-col lg:flex-row justify-between lg:items-start py-7">
-								<div className="mb-5">
+							<div className="flex flex-col lg:flex-row justify-between lg:items-start pb-12">
+								<div className="mb-3">
 									<img
 										src={event1}
 										alt="custom party"
-										className=" w-full h-auto"
+										className="fit-contain"
 									/>
 								</div>
 								<div className="lg:pl-5">
@@ -78,8 +91,8 @@ const Events = () => {
 							</div>
 						</SwiperSlide>
 						<SwiperSlide>
-							<div className="flex flex-col lg:flex-row justify-between lg:items-start">
-								<div className="mb-5">
+							<div className="flex flex-col lg:flex-row justify-between lg:items-start pb-12">
+								<div className="mb-3">
 									<img
 										src={event2}
 										alt="custom party"
@@ -123,8 +136,8 @@ const Events = () => {
 							</div>
 						</SwiperSlide>
 						<SwiperSlide>
-							<div className="flex flex-col lg:flex-row justify-between lg:items-start">
-								<div className="mb-5">
+							<div className="flex flex-col lg:flex-row justify-between lg:items-start pb-12">
+								<div className="mb-3">
 									<img
 										src={event3}
 										alt="custom party"
@@ -169,8 +182,100 @@ const Events = () => {
 						</SwiperSlide>
 					</Swiper>
 				</div>
+				<div className="eventsBg" />
 			</div>
-			<div className="eventsBg" />
+
+			<div className="reservation bg-[#0C0B09] py-20">
+				<div className="container">
+					<div className="title mb-[40px]">
+						<div className="flex flex-row items-center justify-start">
+							<h2 className="text-Poppins textNormal uppercase pr-3">
+								Reservation
+							</h2>
+							<div className="border-t-2 border-[#ffffff33] w-[120px]"></div>
+						</div>
+						<h1 className="font-Play-fair text-[28px] lg:text-[32px]  2xl:text-[36px] text-primary font-bold leading-[54px]">
+							Book a Table
+						</h1>
+					</div>
+					<div className="w-full">
+						<form
+							handleSubmit={handleSubmit}
+							className="flex flex-col justify-center items-center">
+							<div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
+								<input
+									type="text"
+									name="name"
+									id="name"
+									placeholder="Your Name"
+									ref={nameRef}
+									required
+								/>
+								<input
+									type="email"
+									name="email"
+									id="email"
+									placeholder="email"
+									ref={emailRef}
+									required
+								/>
+								<input
+									type="tel"
+									name="phone"
+									id="phone"
+									placeholder="Your Phone"
+									ref={phoneRef}
+									required
+								/>
+								<input
+									type="date"
+									name="date"
+									id="date"
+									placeholder="Date"
+									ref={dateRef}
+									required
+									className="cursor-text"
+								/>
+								<input
+									type="time"
+									id="start"
+									name="trip-start"
+									defaultValue="21:30"
+									ref={timeRef}
+									required
+									className="cursor-text"
+								/>
+								<input
+									type="number"
+									name="people"
+									min="1"
+									max="100"
+									id="people"
+									ref={peopleRef}
+									placeholder="Number of People"
+									required
+								/>
+
+								<textarea
+									type="textarea"
+									className="col-span-1 md:col-span-2 lg:col-span-3 mb-[24px]"
+									ref={messageRef}
+									placeholder="Message (Max Length 500 characters)"
+									rows="5"
+									maxlength="3000"
+								/>
+							</div>
+							<div className="">
+								<button
+									type="submit"
+									className="bg-primary px-[35px] py-[10px] text-[#ffffff] rounded-[50px] cursor-pointer hover:bg-[#d3af71] transition-all ease-in duration-400">
+									Book a Table
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 };
